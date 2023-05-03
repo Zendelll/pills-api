7c5aac1e253f89e24242149a6ddcf22c2cd8169f
Zendell <zendelll@yandex.ru>
2023-05-03 19:22:16 +0300

    added logs

---
+++ internal/logger.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(filename="./logs/pills_api.log",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+logger = logging.getLogger("pills_api")
